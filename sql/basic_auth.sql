114d917a9ef5f86fb64453cdaabbb1f94343efb3
amin <amin.roosta@outlook.com>
2023-05-13 10:20:49 -0400

    include public schema changes

--- sql/basic_auth.sql
+++ sql/basic_auth.sql
@@ -58,3 +58,18 @@ begin
 end;
 $$;
 
+CREATE TYPE jwt_token AS (
+  token text
+);
+
+CREATE FUNCTION jwt_test() RETURNS public.jwt_token AS $$
+  SELECT public.sign(
+    row_to_json(r), 'reallyreallyreallyreallyverysafe'
+  ) AS token
+  FROM (
+    SELECT
+      'my_role'::text as role,
+      extract(epoch from now())::integer + 300 AS exp
+  ) r;
+$$ LANGUAGE sql;
+
